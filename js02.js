e77b81b18f5f4455d01af47b95802acf52dbfa9f
Sami <sachnine@my.waketech.edu>
2026-01-30 14:07:21 -0500

    completed up to page 62

--- js02.js
+++ js02.js
@@ -28,7 +28,7 @@ function setupForm() {
     document.getElementById("makeBook").checked = false;
     document.getElementById("photoRights").checked = false;
     document.getElementById("photoDist").value = 0;
-
+    getEstimate();
 }
 
 // estimate the total cost of the service
@@ -37,11 +37,18 @@ function getEstimate() {
     let photographers = document.getElementById("photoNum").value;
     let hours = document.getElementById("photoHrs").value;
     let distance = document.getElementById("photoDist").value;
+    let buyBook = document.getElementById("makeBook").checked;
+    let buyRights = document.getElementById("photoRights").checked;
 
     // add total cost of photographers for the hours covered
     totalCost += photographers * hours * EMP_COST;
-    
     // add the total cost of distance per photographer per mile
     totalCost += photographers * distance * TRAVEL_COST;
+    // add the cost of the book if purchased
+    totalCost += buyBook ? BOOK_COST : 0;
+    // add the cost of the photo rights if selected
+    totalCost += buyRights ? REPRO_COST : 0;
 
+    // display the total cost estimate
+    document.getElementById("estimate").innerHTML = "$" + totalCost;
 }
